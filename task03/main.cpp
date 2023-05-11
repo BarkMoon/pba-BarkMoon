ef3931ea62580125376420c4cda732c02c3afadc
Shunichiro Ikeno <shunichiro.ikeno@gmail.com>
2023-05-11 12:44:08 +0900

    problem2

--- task03/main.cpp
+++ task03/main.cpp
@@ -172,6 +172,12 @@ void set_force_accelerated(
         } else { // far field approximation
           // write a few lines of code here to compute the force from far grid.
           // use the center for the gravity of the grid : `acc.grid2cg[jy * num_div + jx]`
+
+          // Barnes-Hut simulation.
+          // Assume that a large particle is at the center of the gravity of the particles in a far grid.
+          
+          unsigned int num_particles_in_grid = acc.grid2idx[jy * num_div + jx + 1] - acc.grid2idx[jy * num_div + jx];
+          particles[ip].force += num_particles_in_grid * gravitational_force(acc.grid2cg[jy * num_div + jx] - particles[ip].pos);
         }
       }
     }
@@ -186,7 +192,7 @@ int main() {
   constexpr unsigned int num_div = 30;
 
   // particle information
-  std::vector<Particle> particles(10000); // change the number of particles here
+  std::vector<Particle> particles(20000); // change the number of particles here
   for (auto &p: particles) {
     // initialization
     p.pos.setRandom();
@@ -212,8 +218,8 @@ int main() {
       if( i_step % 20 == 0 ){ std::cout << i_step << " steps in " << num_step << " steps computed" << std::endl; }
 
       // switch brute-force/accelerated computation here by uncomment/comment below
-      set_force_bruteforce(particles);
-      // set_force_accelerated(particles, acceleration, box_size, num_div);
+      // set_force_bruteforce(particles);
+      set_force_accelerated(particles, acceleration, box_size, num_div);
 
       for (auto &p: particles) {
         // leap frog time integration
